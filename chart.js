2d181197a2fd6a12f0dea9087e7e616364458221
raouf bouafia <bouafiar590@gmail.com>
2023-02-25 06:54:00 -0800

    timer

--- chart.js
+++ chart.js
@@ -1,35 +1,24 @@
- const ctx = document.getElementById('myChart').getContext('2d');
-//  Chart.defaults.global.width = 500; // set default width to 500 pixels
-new Chart(ctx, {
-    // Chart.defaults.global.barThickness = 10
-    type: 'bar',
-    data: {
-      labels: [1, 2, 3, 4, 5, 6],
-      datasets: [{
-        label: '# of Votes',
-        data: [[14,20], [8,11], [0,3], [3,7], [0,1], [4,6]],
-        // data: [[0,14], [1,8], [3,10], [1,3], [1,4], 3],
+let year = document.querySelector('.year h2');
+let months = document.querySelector('.months h2');
+let day = document.querySelector('.day h2');
+let hour = document.querySelector('.hour h2');
+let min = document.querySelector('.min h2');
+let sec = document.querySelector('.sec h2');
 
-        borderWidth: 0,
-        barThickness: 5,
-        backgroundColor:'tomato'
-      },
-      {
-        label: '# of Votes',
-        data: [[0,13], [1,7], [4,10], [1,2], [2,4], [0,4]],
-        borderWidth: 0,
-        barThickness: 5,
-        backgroundColor:'dodgerblue'
-      }
-    ],
-      
-    },
+
+setInterval(() => {
+  
+    let time = new Date();
+    year.innerText = time.getFullYear()
+    months.innerText = time.getDay();
+    day.innerText = time.getHours();
+    hour.innerText = time.getHours()
+    min.innerText = time.getMinutes();
+    sec.innerText = time.getSeconds()
     
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+    
+    
+}, 1000);
+
+
+
